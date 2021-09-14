58ad3e088fe672ffe7c61ce35a52176630a136ba
Poliana <81119829+PolianaSouzaAnanias@users.noreply.github.com>
2021-09-14 15:11:20 +0000

    Aplicando getElementById

---
+++ js/script.js
@@ -0,0 +1,5 @@
+if(document.getElementById("nome").value != "" && document.getElementById("email").value != ""){
+  alert("Prontinho! Você receberá as novidades por email.")
+}else{
+  alert("Por favor, preencha os campos nome e email!")
+}
