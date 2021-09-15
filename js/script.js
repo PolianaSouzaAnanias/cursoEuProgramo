d4722ffffead13fbe4a609b9780e59a7ab964b0c
Poliana <81119829+PolianaSouzaAnanias@users.noreply.github.com>
2021-09-15 23:17:53 +0000

    Aplicando Listener (método) que adiciona uma função (sequência de passos).

--- js/script.js
+++ js/script.js
@@ -1,5 +1,13 @@
-if(document.getElementById("nome").value != "" && document.getElementById("email").value != ""){
-  alert("Prontinho! Você receberá as novidades por email.")
-}else{
-  alert("Por favor, preencha os campos nome e email!")
+document.getElementById(botaoEnviar).assEventListener("clic", valodaFormilario);
+
+function validaFormulario(){
+
+  if(document.getElementById("nome").value != "" &&
+     document.getElementById("email").value != "" &&
+     document.getElementById("telefone").value != "" ){
+
+    alert("Prontinho! você receberá as novidades por email.")
+  }else{
+    alert("Por favor, preencha os campos nome e email.")
+  }
 }
